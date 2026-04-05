8a58d898d682eba385333b9ac46d47db4fa96d7f
jairt <ajreyes9@utn.hn>
2026-04-05 15:31:54 -0600

    LoginListo

---
+++ src/Conexion.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    private static final String URL="jdbc:mysql://localhost:3306/rimberio?useSSL=false&serverTimezone=UTC";
+    private static final String USER= "root";
+    private static final String PASSWORD= "";
+    
+    public static Connection conectar(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Conexion Exitosa");
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        }catch(ClassNotFoundException | SQLException e){
+            throw new RuntimeException("Error de Conexion: "+e.getMessage());
+        }
+    }
+    
+    
+}
